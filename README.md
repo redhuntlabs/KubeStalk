d9120e1187b0552081ed70366ebdef41c74e5fde
0xInfection <theinfecteddrake@gmail.com>
2022-10-11 18:34:06 +0530

    added files

---
+++ README.md
@@ -0,0 +1,93 @@
+# KubeStalk
+KubeStalk is a tool to discover Kubernetes and related infrastructure based attack surface from a black-box perspective. This tool is a community version of the tool used to probe for unsecured Kubernetes clusters around the internet during [Project Resonance - Wave 9]().
+
+## Usage
+
+### Installation
+KubeStalk is written in Python and requires the `requests` library.
+
+To install the tool, you can clone the repository to any directory:
+```
+git clone https://github.com/redhuntlabs/kubestalk
+```
+Once cloned, you need to install the `requests` library using `python3 -m pip install requests` or:
+```
+python3 -m pip install -r requirements.txt
+```
+Everything is setup and you can use the tool directly.
+
+### Command-line Arguments
+A list of command line arguments supported by the tool can be displayed using the `-h` flag.
+```s
+$ python3 kubestalk.py  -h
+
+    +---------------------+
+    |  K U B E S T A L K  |
+    +---------------------+   v0.1
+
+[!] HTTPLoot by RedHunt Labs - A Modern Attack Surface (ASM) Management Company
+[!] Author: 0xInfection (RHL Research Team)
+[!] Continuously Track Your Attack Surface using https://redhuntlabs.com/nvadr.
+
+usage: ./kubestalk.py <url(s)>/<cidr>
+
+Required Arguments:
+  urls                  List of hosts to scan
+
+Optional Arguments:
+  -o OUTPUT, --output OUTPUT
+                        Output path to write the CSV file to
+  -f SIG_FILE, --sig-dir SIG_FILE
+                        Signature directory path to load
+  -t TIMEOUT, --timeout TIMEOUT
+                        HTTP timeout value in seconds
+  -ua USER_AGENT, --user-agent USER_AGENT
+                        User agent header to set in HTTP requests
+  --concurrency CONCURRENCY
+                        No. of hosts to process simultaneously
+  --verify-ssl          Verify SSL certificates
+  --version             Display the version of KubeStalk and exit.
+```
+
+#### Basic Usage
+To use the tool, you can pass one or more hosts to the script. All targets passed to the tool must be [RFC 3986](https://datatracker.ietf.org/doc/html/rfc3986) complaint, i.e. must contain a scheme and hostname (and port if required).
+
+A basic usage is as below:
+```
+$ python3 kubestalk.py https://███.██.██.███:10250
+
+    +---------------------+
+    |  K U B E S T A L K  |
+    +---------------------+   v0.1
+
+[!] HTTPLoot by RedHunt Labs - A Modern Attack Surface (ASM) Management Company
+[!] Author: 0xInfection (RHL Research Team)
+[!] Continuously Track Your Attack Surface using https://redhuntlabs.com/nvadr.
+
+[+] Loaded 10 signatures to scan.
+[*] Processing host: https://███.██.██.██:10250
+[!] Found potential issue on https://███.██.██.██:10250: Kubernetes Pod List Exposure
+[*] Writing results to output file.
+[+] Done.
+```
+
+#### HTTP Tuning
+HTTP requests can be fine-tuned using the `-t` (to mention HTTP timeouts), `-ua` (to specify custom user agents) and the `--verify-ssl` (to validate SSL certificates while making requests).
+
+#### Concurrency
+You can control the number of hosts to scan simultanously using the `--concurrency` flag. The default value is set to 5.
+
+#### Output
+The output is written to a CSV filea and can be controlled by the `--output` flag. A sample of the output is as belows:
+
+|host                       |path |issue                       |type          |severity                      |
+|---------------------------|-----|----------------------------|--------------|------------------------------|
+|https://███.██.██.██:10250|/pods|Kubernetes Pod List Exposure|core-component|vulnerability/misconfiguration|
+|https://██.███.██.███:443 |/api/v1/pods|Kubernetes Pod List Exposure|core-component|vulnerability/misconfiguration|
+|https://█.█.██.█:80|/|etcd Viewer Dashboard Exposure|add-on|vulnerability/exposure|
+|https://███.█.██.█:80|/|cAdvisor Metrics Web UI Dashboard Exposure|add-on|vulnerability/exposure|
+
+## Version & License
+The tool is licensed under the [BSD 3 Clause License](LICENSE) and is currently at v0.1.
+
+*[`To know more about our Attack Surface Management platform, check out NVADR.`](https://redhuntlabs.com/nvadr)*
---
+++ kubestalk.py
@@ -0,0 +1,151 @@
+import csv, glob
+import sys, warnings
+import requests, re, json
+from urllib.parse import urljoin
+from argparse import ArgumentParser
+from concurrent.futures import ThreadPoolExecutor
+
+version = 'v0.1'
+writer = None
+fingerprints = list()
+def_headers = {
+    'User-Agent'        : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+    'Accept'            : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Language'   : 'en-US,en;q=0.9',
+    'Accept-Encoding'   : 'identity',
+    'Sec-Fetch-Mode'    : 'navigate',
+    'DNT'               : '1',
+    'Connection'        : 'close'
+}
+lackofart = '''
+    +---------------------+
+    |  K U B E S T A L K  |
+    +---------------------+   %s
+
+[!] HTTPLoot by RedHunt Labs - A Modern Attack Surface (ASM) Management Company
+[!] Author: 0xInfection (RHL Research Team)
+[!] Continuously Track Your Attack Surface using https://redhuntlabs.com/nvadr.
+'''
+warnings.filterwarnings('ignore')
+
+def loader(fname: str) -> None:
+    '''
+    Loads the signatures from JSON
+    '''
+    global fingerprints
+    files = glob.glob(fname+'/*.json')
+    for xfile in files:
+        with open(xfile, 'r') as rf:
+            fingerprints.append(json.load(rf))
+    print('[+] Loaded %d signatures to scan.' % len(fingerprints))
+
+def make_request(host: str, path: str, timeout: int, ssl=False) -> str:
+    '''
+    Makes a HTTP request to the URL specified
+    '''
+    xurl = urljoin(host, path)
+    req = requests.get(xurl, headers=def_headers, timeout=timeout, verify=ssl)
+    if req is not None:
+        return req.text
+
+def match_fps(path: str, resp: str) -> tuple:
+    '''
+    Matches the fingerprints per path
+    '''
+    for x in fingerprints:
+        if isinstance(x['path'], list):
+            for path in x['path']:
+                for rex in x['detector']:
+                    if re.search(rex, resp, re.I):
+                        return x['name'], x['type'], x['severity']
+        else:
+            if x['path'] == path:
+                for rex in x['detector']:
+                    if re.search(rex, resp, re.I):
+                        return x['name'], x['type'], x['severity']
+
+def proc_host(host: str, timeout: int, ssl: bool) -> None:
+    '''
+    Processes a single host
+    '''
+    print('[*] Processing host:', host)
+    global writer
+    allpaths = list()
+
+    for fp in fingerprints:
+        if isinstance(fp['path'], list):
+            allpaths.extend(fp['path'])
+        else:
+            allpaths.append(fp['path'])
+
+    if '://' not in host:
+        print('[-] Scheme not specified, fixing protocol with http (might not work correctly)...')
+        host = 'http://' + host
+
+    for path in set(allpaths):
+        resp = make_request(host, path, timeout, ssl)
+        xfresp = match_fps(path, resp)
+
+        if xfresp:
+            print('[!] Found potential issue on %s: %s' % (host, xfresp[0]))
+            writer.writerow([host, path, xfresp[0], xfresp[1], xfresp[2]])
+
+def process_hosts(hosts: list, concurrency: int, timeout: int, ssl: bool) -> None:
+    '''
+    Main wrapper around the engine
+    '''
+    with ThreadPoolExecutor(max_workers=concurrency) as exec:
+        for host in hosts:
+            exec.submit(proc_host, str(host), timeout, ssl)
+
+def main():
+    '''
+    Main function to wrap all of them
+    '''
+    print(lackofart % version)
+    parser = ArgumentParser(usage='./kubestalk.py <url(s)>/<cidr>')
+    parser._action_groups.pop()
+
+    required = parser.add_argument_group('Required Arguments')
+    optional = parser.add_argument_group('Optional Arguments')
+
+    required.add_argument('urls', nargs='*', help='List of hosts to scan')
+
+    optional.add_argument('-o', '--output', help='Output path to write the CSV file to', dest='output', default='kubestalk-results.csv')
+    optional.add_argument('-f', '--sig-dir', help='Signature directory path to load', dest='sig_dir', default='plugins')
+    optional.add_argument('-t', '--timeout', help='HTTP timeout value in seconds', dest='timeout', type=int, default=10)
+    optional.add_argument('-ua', '--user-agent', help='User agent header to set in HTTP requests', dest='user_agent')
+    optional.add_argument('--concurrency', help='No. of hosts to process simultaneously', dest='concurrency', type=int, default=5)
+    optional.add_argument('--verify-ssl', help='Verify SSL certificates', dest='ssl', action='store_true', default=False)
+    optional.add_argument('--version', help='Display the version of KubeStalk and exit.', dest='version', action='store_true')
+
+    args = parser.parse_args()
+
+    if not args.urls and not args.version:
+        print('[-] You must supply at least a single host to scan.')
+        sys.exit(1)
+
+    if args.version:
+        print('[+] KubeStalk Version:', version)
+        sys.exit()
+
+    if args.sig_dir:
+        loader(args.sig_dir)
+
+    global writer, def_headers
+    xfwriter = open(args.output, 'w+')
+    writer = csv.writer(xfwriter)
+    writer.writerow(['host', 'path', 'issue', 'type', 'severity'])
+
+    if args.user_agent:
+        def_headers['User-Agent'] = args.user_agent
+
+    process_hosts(args.urls, args.concurrency, args.timeout, args.ssl)
+
+    print('[*] Writing results to output file.')
+    xfwriter.flush()
+    xfwriter.close()
+    print('[+] Done.')
+
+if __name__ == '__main__':
+    main()
